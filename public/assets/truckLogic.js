166e4a2b2d60ad14b9fb1161ca4c4b550f830d00
Pholtzman88 <holtzman.patrick@gmail.com>
2017-08-08 13:40:48 -0500

    update cart

--- public/assets/truckLogic.js
+++ public/assets/truckLogic.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+	//modal control
 	$('.modal').modal();
-})
+	$(".addToCartBtn").on("click", function(){
+		console.log(this.id);
+		sessionStorage.setItem("foodName",this.id);
+	});
+	$("#cartBtn").on("click", function(){
+		var menuItem = "<div>";
+		menuItem += sessionStorage.getItem("foodName");
+		menuItem += "</div>";
+		$(".modal-content").append(menuItem);
+	});
+});
