da29bcc55087611211be2b1fc84869873300188f
Pholtzman88 <holtzman.patrick@gmail.com>
2017-08-08 01:25:15 -0500

    update handlebars for browsing

--- controller/controller.js
+++ controller/controller.js
@@ -21,9 +21,15 @@ router.get("/logIn", function(req,res){
 });
 
 router.post("/signUp/create", function(req,res){
-	var user = req.body;
-	Client.create(user).then(function(data){
-		res.redirect("/");
+
+	// Client.create(user).then(function(data){
+		res.redirect("/browse");
+	// });
+});
+router.get("/browse",function(req,res){
+	Client.findAll({
+	}).then(function(data){
+		res.render("browse", { foodTruck: data});
 	});
 });
 
@@ -32,26 +38,3 @@ module.exports = router;
 
 
 	////example of how we can interact with data from JSON in info column
-	Client.findAll({
-		where:{
-			id: 1
-		},
-		raw: true
-	}).then(function(data){
-		var obj = data[0].info;
-		var data = JSON.parse(obj);
-		var menuLength = data.menu.length;
-		console.log("vendor info");
-		console.log("----------------------------");
-		console.log("name: " + data.name);
-		console.log("location: " + data.location);
-		console.log("phone number: " + data.phoneNumber);
-		console.log("menu");
-		console.log("****************************************");
-		for (i=0;i < menuLength; i++){
-			console.log("food name: "+data.menu[i].name);
-			console.log("food description: " + data.menu[i].description);
-			console.log("food price: " + data.menu[i].price);
-			console.log("---------------------------------------");
-		};
-	});
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
-CREATE DATABASE foodtruck;
-USE foodtruck;
+CREATE DATABASE foodtruck_db;
+USE foodtruck_db;
 CREATE TABLE clients (
 	id INTEGER(100) AUTO_INCREMENT PRIMARY KEY,
-	info VARCHAR(10000),
-	vendor BOOLEAN,
+	companyName VARCHAR(100),
 	clientEmail VARCHAR(100),
-	clientPassword VARCHAR(100)
+	clientPassword VARCHAR(100),
+	phoneNumber VARCHAR(100),
+	description VARCHAR(100),
+	location VARCHAR(100),
+	menu VARCHAR(1000)
 );
