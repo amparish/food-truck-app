2f7e79bda955e4db62c2aed5c9e633f0b56fa1db
Pholtzman88 <holtzman.patrick@gmail.com>
2017-08-10 16:04:38 -0500

    update cart

--- public/assets/truckLogic.js
+++ public/assets/truckLogic.js
@@ -4,19 +4,17 @@ $(document).ready(function(){
 	var counter = 0;
 	var id=0;
 	var totalArr = [];
-	
+	var cart = [];
 
 	$(".addToCartBtn").on("click", function(e){
 		e.preventDefault();
 			var total = 0;
-			console.log(this.name)
 			var str = this.name;
 			var arr = str.split(",");
 			var fn = arr[0];
 			var fd = arr[1];
 			var p = arr[2];
 			var obj = '{"foodName":"'+fn+'","foodDescription":"'+fd+'","price":"'+p+'","id":"'+id+'"}';
-			console.log(obj)
 			sessionStorage.setItem("menuItem",obj);
 			var item = sessionStorage.getItem("menuItem");
 			var cartItem = JSON.parse(item);
@@ -24,21 +22,24 @@ $(document).ready(function(){
 			totalArr.push(parsedPrice);
 			for(var i=0; i < totalArr.length; i++){
 				total += totalArr[i];
-				console.log(totalArr[i])
 			}
 			var tax = total * .0825;
 			tax = parseFloat(tax);
 			var totalSale = tax + total;
 			totalSale = totalSale.toFixed(2);
 			tax = tax.toFixed(2);
-			console.log(totalSale);
+			var key = cartItem.id;
+			var name = cartItem.foodName;
+			var description = cartItem.foodDescription;
+			var price = cartItem.price;
 			
 			//sets up list of items in cart
-			var display= "<table id="+cartItem.id+">";
+			var display= "<table id="+key+">";
 			display += "<tr>";
-			display += "<td class='col s4'>"+cartItem.foodName+"</td>";
-			display += "<td class='col s6'>"+cartItem.foodDescription+"</td>";
-			display += "<td class='col s2'>"+"$"+cartItem.price+"</td>";
+			display += "<td class='col s4'>"+name+"</td>";
+			display += "<td class='col s6'>"+description+"</td>";
+			display += "<td class='col s2'>"+"$"+price;
+			display += "<button class='delete' value="+key+"><i class='material-icons'>delete</i></button></td>";
 			display += "</tr>";
 			display += "</table>";
 			$("#itemList").append(display);
@@ -57,13 +58,20 @@ $(document).ready(function(){
 				$("#cartCounter").addClass("hidden");
 			}
 			id+=1;
+			cart.push({key,name,description,price});
 	});
-
 	//this will actually be for delete just testing logic
-	$(".navBtn").on("click", function(e){
-		e.preventDefault();
-		counter = counter -1;
-					if (counter > 1){
+	console.log(cart)
+
+
+		// $(".delete").on("click", function(){
+		// 	console.log("i can delete!" + this)
+		// })
+		$(document).on("click",".delete",function(){
+			console.log(this.value)
+			$("#"+this.value).remove();
+			counter = counter -1;
+			if (counter > 1){
 				$("#cartCounter").html("+"+counter);
 			}else if (counter > 0){
 				$("#cartCounter").html("+"+counter);
@@ -71,5 +79,13 @@ $(document).ready(function(){
 			}else{
 				$("#cartCounter").addClass("hidden");
 			}
+		})
+
+	$("#cartNavBtn").on("click", function(e){
+		e.preventDefault();
+		console.log(cart);
+		// $(document).on("click","#delete"+cart.i.key,function(){
+		// 	console.log("i can delete this")
+		// })
 	})
 });
